7b6759110b62def737201cadc55dd20b929640e3
Jithesh M <jitheshmjithooz@gmail.com>
2023-08-29 22:49:30 +0530

    pages link added in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 import styles from '../../styles/Navbar.module.css'
 function Navbar() {
   return (
     <div className={`${styles.navbar} navbar px-3`}>
       <div className={styles.iconContainer}>
-        <div className={styles.icon}><i className="fas fa-home" /><span className={styles.iconName}>Home</span></div>
-        <div className={styles.icon}><i className="fas fa-user" /><span className={styles.iconName}>Personal</span></div>
-        <div className={styles.icon}><i className="fas fa-lightbulb" /><span className={styles.iconName}>Skills</span></div>
-        <div className={styles.icon}><i className="fas fa-book-open" /><span className={styles.iconName}>Education</span></div>
-        <div className={styles.icon}><i className="fas fa-desktop" /><span className={styles.iconName}>Projects</span></div>
+      <Link href="/"> <div className={styles.icon}><i className="fas fa-home" /><span className={styles.iconName}>Home</span></div></Link>
+      <Link href="/about"> <div className={styles.icon}><i className="fas fa-user" /><span className={styles.iconName}>Personal</span></div></Link>
+      <Link href="/skills"> <div className={styles.icon}><i className="fas fa-lightbulb" /><span className={styles.iconName}>Skills</span></div></Link>
+      <Link href="/education"> <div className={styles.icon}><i className="fas fa-book-open" /><span className={styles.iconName}>Education</span></div></Link>
+      <Link href="/projects"> <div className={styles.icon}><i className="fas fa-desktop" /><span className={styles.iconName}>Projects</span></div></Link>
       </div>
     </div>
 
