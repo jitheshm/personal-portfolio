ffee679169ee176c24e18eec05f769a897f50bd2
Jithesh M <jitheshmjithooz@gmail.com>
2023-09-14 20:52:07 +0530

    small bug corrected and conditionally check the request method

--- src/pages/api/update-project.js
+++ src/pages/api/update-project.js
@@ -1,53 +1,56 @@
-import { addProject, getProjects, updateProject } from '@/helpers/helper';
+import { updateProject } from '@/helpers/helper';
 import { IncomingForm } from 'formidable';
 import cloudinary from '@/lib/cloudinary'
 
 export const config = {
   api: {
     bodyParser: false, // Disable the default JSON body parser
+    externalResolver: true
   },
 };
 
 export default async function handler(req, res) {
-  console.log(req.query.pid);
-  const form = new IncomingForm();
+  if (req.method === 'POST') {
+    console.log(req.query.pid);
+    const form = new IncomingForm();
 
-  form.parse(req, async (error, fields, files) => {
-    
-    const data = {
-      name: fields.name[0],
-      category: fields.category[0],
-      description: fields.description[0],
-      language: fields.language[0],
-      github: fields.github[0],
-      host: fields.host[0],
-    };
-    console.log(data);
-    const { image } = files;
+    form.parse(req, async (error, fields, files) => {
+
+      const data = {
+        name: fields.name[0],
+        category: fields.category[0],
+        description: fields.description[0],
+        language: fields.language[0],
+        github: fields.github[0],
+        host: fields.host[0],
+      };
+      console.log(data);
+      const { image } = files;
 
 
-    if (image) {
-      updateProject(req.query.pid, data).then((response) => {
-        console.log(response);
+      if (image) {
+        updateProject(req.query.pid, data).then((response) => {
+          console.log(response);
 
-        return cloudinary.uploader.upload(`${image[0].filepath}`, {
-          public_id: response.imagePublic_id,
+          return cloudinary.uploader.upload(`${image[0].filepath}`, {
+            public_id: response.imagePublic_id,
+          })
+
+        }).then((result) => {
+          res.status(200).json({ message: 'Project updated successfully.' });
+        }).catch(() => {
+          res.status(400).json({ error: 'failed' });
         })
-
-      }).then((result) => {
-        res.status(200).json({ message: 'Project updated successfully.' });
-      }).catch(() => {
-        res.status(400).json({ error: 'failed' });
-      })
-    } else {
-      updateProject(req.query.pid, data).then((result) => {
-        res.status(200).json({ message: 'Project updated successfully.' });
-      }).catch(() => {
-        res.status(400).json({ error: 'failed' });
-      })
-    }
+      } else {
+        updateProject(req.query.pid, data).then((result) => {
+          res.status(200).json({ message: 'Project updated successfully.' });
+        }).catch(() => {
+          res.status(400).json({ error: 'failed' });
+        })
+      }
 
 
 
-  });
+    });
+  }
 }
