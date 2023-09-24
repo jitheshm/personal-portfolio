8a8fde4a2acc4d3353ada62b33f1be3ac9205fe1
Jithesh M <jitheshmjithooz@gmail.com>
2023-09-24 17:24:25 +0530

    error corrected in redirection after signin

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -58,6 +58,13 @@ export default NextAuth({
        console.log(session);
       return session
     },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    }
     
   }
   
