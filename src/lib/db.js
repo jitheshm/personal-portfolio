54642adb4ef083259a30282e8d0491edf6bb0d16
Jithesh M <jitheshmjithooz@gmail.com>
2023-09-24 18:40:48 +0530

    database connection updated

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,23 @@
 const { default: clientPromise } = require("./mongodb");
-
+var dbPack;
 module.exports = {
     getDb: () => {
-        return new Promise(async(resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             try {
-                const client = await clientPromise;
-                const db = client.db("portfolio-website");
-                resolve(db);
+                if (dbPack) {
+                    console.log("database already connected");
+                    resolve(dbPack)
+                } else {
+                    console.log("connecting to database");
+                    const client = await clientPromise;
+                    const db = client.db("portfolio-website");
+                    dbPack = db;
+                    resolve(dbPack);
+                }
             } catch (error) {
                 console.error("Error connecting to MongoDB:", error);
                 reject(error);
             }
         })
-       }
+    }
 }
